55313a8e1999dd2e29426c328bbe52ae69ac18d4
Mst.Sabina Yasmin <164632074+sabina9797@users.noreply.github.com>
2024-11-16 23:50:26 +0600

    Add files via upload

---
+++ Week4/Day1/RCB_VS_CSK.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   //for fast input output
+   ios ::sync_with_stdio(false) ;
+   cin.tie(nullptr) ;   
+   int x,y;
+   cin >> x >> y;
+   if(x-y>=18)
+   {
+    cout << "RCB" << '\n';
+   }else{
+     cout << "CSK" << '\n';
+   }
+ 
+  return 0;
+}
+ 
