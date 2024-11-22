6094f121d762830050e2808db0b9ee0bf16a92fd
Mst.Sabina Yasmin <164632074+sabina9797@users.noreply.github.com>
2024-11-22 22:12:45 +0600

    Add files via upload
    Added week4/day6__

---
+++ Week4/Day1/Day6__/Number_of_segment_with_big_sum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int n;
+   long long k;
+   cin >> n >> k;
+   vector<int> a(n);
+   for (int i = 0;i < n;i++) {
+      cin >> a[i];
+   }
+
+   int l = 0, r = 0;
+   long long sum = 0, ans =0;
+   while (r < n) {
+      sum += a[r];
+      
+      
+      while(sum>=k && l<=r) 
+      {
+             ans += n-r;
+              sum -= a[l];
+            l++;
+      }
+      r++;
+
+      }
+         cout << ans << '\n';
+        
+   
+        
+        return 0;
+      }
+      
+   
+
