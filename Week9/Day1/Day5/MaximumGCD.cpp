20fb142dd3b98283fc9adf1c9e16c62cd844e63c
Mst.Sabina Yasmin <164632074+sabina9797@users.noreply.github.com>
2024-12-26 20:40:28 +0600

    Update MaximumGCD.cpp

--- Week9/Day1/Day5/MaximumGCD.cpp
+++ Week9/Day1/Day5/MaximumGCD.cpp
@@ -12,30 +12,7 @@ int main()
         long long n;
         cin >> n;
         cout<< n/2<<endl;
-        /*vector<pair<long long,long long>>v;
-        long long GCD, mx =0;
-        for(long long i=1; i<=n; i++)
-        {
-            for(long long j=i+1; j<=n; j++)
-        {
-            GCD = __gcd(i,j);
-        
-        if(GCD > mx)
-        {
-            mx = GCD;
-            v.clear();
-            v.push_back({i,j});
-        }else if(GCD == mx)
-        {
-            v.push_back({i,j});
-        }
-
-        }
-        
-
-   }
-   cout<<mx<<endl;
-   }*/
+       
    }
   return 0;
 }
